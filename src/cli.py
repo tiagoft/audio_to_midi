a46efaac5087d250a823b7629dda6315562db5aa
tiagoft <tiagoft@gmail.com>
2022-10-01 16:35:25 -0300

    Made CLI comply with parameter names

--- src/cli.py
+++ src/cli.py
@@ -6,15 +6,18 @@ import librosa
 
 from audio2midi import wave_to_midi
 
-if __name__ == "__main__":
+def run():
     print("Starting...")
     file_in = sys.argv[1]
     file_out = sys.argv[2]
     y, sr = librosa.load(file_in, sr=None)
     print("Audio file loaded!")
-    midi = wave_to_midi(y, sr=sr)
+    midi = wave_to_midi(y, srate=sr)
     print("Conversion finished!")
     with open (file_out, 'wb') as f:
         midi.writeFile(f)
     print("Done. Exiting!")
-    
+
+
+if __name__ == "__main__":
+    run()
