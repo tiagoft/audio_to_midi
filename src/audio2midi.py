6c58251407ff54bddd88806952aefd4f505724e0
tiagoft <tiagoft@gmail.com>
2022-10-01 11:18:31 -0300

    Reviewed pydocs for prior_probabilities()

--- src/audio2midi.py
+++ src/audio2midi.py
@@ -5,7 +5,7 @@ import librosa
 import midiutil
 import sys
 
-def transition_matrix(note_min : str, note_max: str, p_stay_note: float, p_stay_silence: float) -> np.array( 2*N_notes+1, 2*N_notes+1):
+def transition_matrix(note_min : str, note_max: str, p_stay_note: float, p_stay_silence: float) -> np.array():
     """
     Returns the transition matrix with one silence state and two states
     (onset and sustain) for each note. This matrix mixes an acoustic model with two states with
@@ -61,7 +61,85 @@ def transition_matrix(note_min : str, note_max: str, p_stay_note: float, p_stay_
     return T
 
 
-def states_to_pianoroll(states : list(int), note_min : str, note_max : str, hop_time : float) -> list:
+def prior_probabilities(y : np.array(), note_min : str, note_max : str, sr : int, frame_length : int=2048, hop_length : int=512, pitch_acc : float=0.9, voiced_acc : float=0.9, onset_acc : float=0.9, spread : float=0.2):
+    """
+    Estimate prior (observed) probabilities from audio signal    
+
+    Parameters
+    ----------
+    y : 1-D numpy array
+        Array containing audio samples
+        
+    note_min : string, 'A#4' format
+        Lowest note supported by this estimator
+    note_max : string, 'A#4' format
+        Highest note supported by this estimator
+    sr : int
+        Sample rate.
+    frame_length : int 
+    window_length : int
+    hop_length : int
+        Parameters for FFT estimation
+    pitch_acc : float, between 0 and 1
+        Probability (estimated) that the pitch estimator is correct.
+    voiced_acc : float, between 0 and 1
+        Estimated accuracy of the "voiced" parameter.
+    onset_acc : float, between 0 and 1
+        Estimated accuracy of the onset detector.
+    spread : float, between 0 and 1
+        Probability that the singer/musician had a one-semitone deviation
+        due to vibrato or glissando.
+
+    Returns
+    -------
+    P : 2D numpy array.
+        P[j,t] is the prior probability of being in state j at time t.
+
+    """
+    
+    fmin = librosa.note_to_hz(note_min)
+    fmax = librosa.note_to_hz(note_max)
+    midi_min = librosa.note_to_midi(note_min)
+    midi_max = librosa.note_to_midi(note_max)
+    n_notes = midi_max - midi_min + 1
+    
+    # F0 and voicing
+    f0, voiced_flag, voiced_prob = librosa.pyin(y, fmin*0.9, fmax*1.1, sr, frame_length, int(frame_length/2), hop_length)
+    tuning = librosa.pitch_tuning(f0)
+    f0_ = np.round(librosa.hz_to_midi(f0-tuning)).astype(int)
+    onsets = librosa.onset.onset_detect(y, sr=sr, hop_length=hop_length, backtrack=True)
+
+
+    P = np.ones( (n_notes*2 + 1, len(f0)) )
+
+    for t in range(len(f0)):
+        # probability of silence or onset = 1-voiced_prob
+        # Probability of a note = voiced_prob * (pitch_acc) (estimated note)
+        # Probability of a note = voiced_prob * (1-pitch_acc) (estimated note)
+        if voiced_flag[t]==False:
+            P[0,t] = voiced_acc
+        else:
+            P[0,t] = 1-voiced_acc
+
+        for j in range(n_notes):
+            if t in onsets:
+                P[(j*2)+1, t] = onset_acc
+            else:
+                P[(j*2)+1, t] = 1-onset_acc
+
+            if j+midi_min == f0_[t]:
+                P[(j*2)+2, t] = pitch_acc
+
+            elif np.abs(j+midi_min-f0_[t])==1:
+                P[(j*2)+2, t] = pitch_acc * spread
+
+            else:
+                P[(j*2)+2, t] = 1-pitch_acc
+
+    return P
+
+
+def states_to_pianoroll(states : list(int), note_min : str, note_max : str, hop_time : float) -> list():
     """
     Converts state sequence to an intermediate, internal piano-roll notation
 
